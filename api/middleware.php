d7f8fe1915ec257d6d05dc7e5da093393a95c87f
jaehwa <kjhwert11@naver.com>
2020-06-21 09:04:10 +0900

    foreach -> map

--- api/middleware.php
+++ api/middleware.php
@@ -5,9 +5,9 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 spl_autoload_register(function ($class) {
     $dirs = ['/','/model/','/controller/'];
 
-    foreach ($dirs as $dir) {
+    array_map(function ($dir) use ($class) {
         if (file_exists(__DIR__."{$dir}{$class}.php")) {
             require_once __DIR__."{$dir}{$class}.php";
         }
-    }
+    }, $dirs);
 });
