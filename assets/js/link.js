c7664b079976cc1b02f56614931d9d8596c48d17
hlab <contact@hlabtech.com>
2020-06-23 16:32:25 +0900

    20200623

---
+++ assets/js/link.js
@@ -0,0 +1,4 @@
+function main(){window.location.href="../main/main.html"}; //메인화면
+
+function material_status(){window.location.href="../product_history/material_status.html"}; //원자재 생산현황
+function material_create(){window.location.href="../product_history/material_create.html"}; //원자재 등록
